856d965852058977e1283acd74445687e1cc0223
khiman7 <kyrylkhiman@gmail.com>
2024-03-29 16:46:10 +0200

    feat(client): add redirect to the feed by clicking on the header logo

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,11 +1,16 @@
 import { Grid, Typography } from '@mui/joy';
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from '../../constants';
 
 export default function Header() {
   return (
     <Grid container component="header" sx={{ py: 3, px: 6 }}>
-      <Typography level="h3" fontWeight="bold">
-        📰 RSS Parser
-      </Typography>
+      <Link style={{ textDecoration: 'none' }} to={ROUTES.ROOT}>
+        <Typography level="h3" fontWeight="bold">
+          📰 RSS Parser
+        </Typography>
+      </Link>
     </Grid>
   );
 }
